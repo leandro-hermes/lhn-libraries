ec9d8bb8c7de1010da9304e6726565e6395668e2
Leandro Hermes <leandro@gdoor.com.br>
2019-12-09 10:50:41 -0300

    updt.: added option for disable close clicking outside or pressing Esc

--- projects/mat-alert/src/lib/mat-alert.service.ts
+++ projects/mat-alert/src/lib/mat-alert.service.ts
@@ -12,13 +12,20 @@ export class MatAlert {
 
   /**
    * Opens an alert dialog
+   *
+   * @param title Title for the alert
+   * @param message Message body. Can be an additional description
+   * @param config Configuration for the alert
+   * @param disableClose Flag for disable closing the alert clicking outside or pressing Escape
    */
-  public show(title: string, message: string = '', config: Partial<MatAlertConfig> = {}): Observable<void> {
+  public show(title: string, message: string = '', config: Partial<MatAlertConfig> = {}, disableClose = false): Observable<void> {
     config.title = title;
     config.message = message;
     return this.dlg.open(MatAlertComponent, {
       minWidth: 300,
       data: config,
+      role: 'alertdialog',
+      disableClose,
     }).afterClosed();
   }
 }
