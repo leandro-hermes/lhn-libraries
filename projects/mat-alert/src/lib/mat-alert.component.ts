897cd74b6ef8ab52bd32058117434fc57f904141
Leandro Hermes <leandro@gdoor.com.br>
2019-12-09 13:32:43 -0300

    updt.: handle linebreaks from pure string

--- projects/mat-alert/src/lib/mat-alert.component.ts
+++ projects/mat-alert/src/lib/mat-alert.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 import { MatAlertConfig } from './mat-alert-config.interface';
 import { MAT_ALERT_DEFAULT_CONFIG } from './mat-alert-default-config.constant';
@@ -15,10 +15,17 @@ export class MatAlertComponent {
   public config: MatAlertConfig;
 
   constructor(@Inject(MAT_DIALOG_DATA) data: MatAlertConfig,
-              sanitizer: DomSanitizer,
+              private sanitizer: DomSanitizer,
               @Inject(MAT_ALERT_DEFAULT_CONFIG) defaultConfigs: MatAlertConfig) {
-    data.message = sanitizer.bypassSecurityTrustHtml(data.message.toString());
+    data.message = this._handleMessage(data.message);
     this.config = Object.assign({}, defaultConfigs, data);
   }
 
+  /**
+   * Handles the message and sanitize
+   */
+  private _handleMessage(message: string | SafeHtml) {
+    message = message.toString().replace(/(?:\r\n|\r|\n)/g, '<br/>');
+    return this.sanitizer.bypassSecurityTrustHtml(message.toString());
+  }
 }
