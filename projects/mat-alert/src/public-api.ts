705a672aa2e1ee59edc3c0ac93dbd995fb314d7c
Leandro Hermes <leandro@gdoor.com.br>
2020-05-14 10:03:38 -0300

    update to Angular 9

--- projects/mat-alert/src/public-api.ts
+++
@@ -1,6 +0,0 @@
-/*
- * Public API Surface of mat-alert
- */
-
-export * from './lib/mat-alert.service';
-export * from './lib/mat-alert.module';
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {path: 'mat-alert', loadChildren: () => import('./mat-alert-demo/mat-alert-demo.module').then(m => m.MatAlertDemoModule)},
-];
+
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule { }
